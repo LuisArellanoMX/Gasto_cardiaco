96c71b2f5b4259eddcd176169852843875c10bf7
LuisArellanoMX <19240062@leon.tecnm.mx>
2021-12-03 00:18:09 -0600

    Se agregaron las animaciones y el sonido

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,47 @@
-let r_fick = document.getElementById("resultado_nico");
+let r_fick = document.getElementById("resultado_nico")
 let r_termo_c = document.getElementById("resultado_termo_c")
 let r_termo_p = document.getElementById("resultado_termo_p")
 
 
-let boton = document.getElementById("boton_calcular");
+let boton = document.getElementById("boton_calcular")
 
 boton.addEventListener("click",run)
 
 function run(){
-    r_fick.innerHTML = calcularFick();
-    r_termo_c.innerHTML = calcularTermoC()
-    r_termo_p.innerHTML = calcularTermoP()
+    cargarResultados()
+    setTimeout(function(){
+        r_fick.innerHTML = calcularFick();
+        r_termo_c.innerHTML = calcularTermoC()
+        r_termo_p.innerHTML = calcularTermoP()
+    }, 15000)
+    
+    mostrarAnimaciones()
 }
 
+function cargarResultados(){
+    r_fick.innerHTML = "Cargando..."
+    r_termo_c.innerHTML = "Cargando..."
+    r_termo_p.innerHTML = "Cargando..."
+}
+
+
+
+function mostrarAnimaciones(){
+    document.querySelector("#anim01").style.display = 'inline-block'
+    document.querySelector("#anim02").style.display = 'inline-block'
+    document.querySelector("#anim03").style.display = 'inline-block'
+    
+    document.getElementById("latidos").play()
+    document.getElementById("monitor").play()
+    setTimeout(function(){
+        ocultarAnimaciones()
+    }, 15500)
+}
+
+function ocultarAnimaciones(){
+    document.querySelector("#anim01").style.display = 'none'
+    document.querySelector("#anim02").style.display = 'none'
+    document.querySelector("#anim03").style.display = 'none'
+}
 
 
